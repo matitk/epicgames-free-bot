424252a1e22ce95568f718b3b88e3cfcda11937b
matitk <matitk@gmail.com>
2023-01-07 22:10:26 -0300

    Add main file

---
+++ index.js
@@ -0,0 +1,14 @@
+import scrape from './scraper/epicgames.js'
+import sendMessage from './telegram/send_message.js'
+
+async function main () {
+  const games = scrape()
+  const fmtGames = (await games).map(el => {
+    return `<b>${el.title}</b>\n${el.description}\n${el.url}\n<i>Until ${el.date.end_date.toDateString()}</i>`
+  })
+  fmtGames.forEach(el => {
+    sendMessage(el)
+  })
+}
+
+main()
