cdbd94f26b78309691fdba6699594c9c5fd0c541
matitk <matitk@gmail.com>
2023-01-08 01:31:01 -0300

    Change space between formatted text message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import sendMessage from './telegram/send_message.js'
 async function main () {
   const games = await scrape()
   const fmtGames = games.map(el => {
-    return `<b>${el.title}</b>\n${el.description}\n${el.url}\n<i>Until ${el.date.end_date.toDateString()}</i>`
+    return `<b>${el.title}</b>\n${el.description}\n\n${el.url}\n\n<i>Until ${el.date.end_date.toDateString()}</i>`
   })
   fmtGames.forEach(el => {
     sendMessage(el)
