14223f0bc462abf20329a39aad0b2a292baf12c2
matitk <matitk@gmail.com>
2023-01-08 01:28:34 -0300

    Refactor async-await for better readability

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import scrape from './scraper/epicgames.js'
 import sendMessage from './telegram/send_message.js'
 
 async function main () {
-  const games = scrape()
-  const fmtGames = (await games).map(el => {
+  const games = await scrape()
+  const fmtGames = games.map(el => {
     return `<b>${el.title}</b>\n${el.description}\n${el.url}\n<i>Until ${el.date.end_date.toDateString()}</i>`
   })
   fmtGames.forEach(el => {
